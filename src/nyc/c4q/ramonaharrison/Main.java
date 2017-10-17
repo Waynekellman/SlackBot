8974b751e42b5479869157975179674464d09073
Wayne Kellman <waynekellman@ac.c4q.nyc>
2017-10-16 20:50:48 -0400

    SlackBotHW

--- src/nyc/c4q/ramonaharrison/Main.java
+++ src/nyc/c4q/ramonaharrison/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
         // Post "Hello, world!" to the #bots channel
         //myBot.sendMessage("Hello, world!");
+        //I want to see how this works
 
         // Post a pineapple photo to the #bots channel
         //myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
